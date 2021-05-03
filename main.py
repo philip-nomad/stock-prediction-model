e7392748ed928c98364cad91e6f2fd521a31f7ee
KTH-INHA-16 <kim1120318@naver.com>
2021-05-03 14:42:26 +0900

    5.3 감성분석 산수식 변경 및 단어 추출을 csv로 따로 뺌 + 비율 및 점수를 또다른 디렉토리로 뺌

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import news_contents_crawler as crawler
 import news_contents_sentiment as sentiment
 import preprocess_kosac as preprocess
 
-# 삼전, SK하이닉스, NAVER, 카카오
-company_code_list = ['005930', '000660', '035420', '035720']
+# 삼전, SK하이닉스, NAVER, 카카오, 현대차, LG, SK, KT, 넷마블, 셀트리온, LG 화학, LG생활건강, 기아, 삼성전기, 이마트
+company_code_list = ['005930', '000660', '035420', '035720','005380','003550','034730','030200','251270','068270','051910','051900','000270','009150','139480','008770']
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # start crawling
-    crawler.start(company_code_list)
+    #crawler.start(company_code_list)
     for company_code in company_code_list:
         preprocess.start(company_code)
         sentiment.text_processing(company_code)
