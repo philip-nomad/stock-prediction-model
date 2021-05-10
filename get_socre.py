27e048f6cdff5cb6ed501361739db1fb6c3f1ef0
Jaehyung <godjaehyung@gmail.com>
2021-05-10 21:23:03 +0900

    make_criteria

---
+++ get_socre.py
@@ -0,0 +1,18 @@
+import preprocessor_criteria
+import news_crawler_criteria
+import sentimental_analysis_criteria
+import make_criteria
+#삼성전자:005930
+#카카오:035720
+#네이버:035420
+#하이닉스:000660
+#셀트리온:068270
+company_code = '068270'
+days = 17
+wherep = 5
+news_crawler_criteria.start(company_code,days,wherep)
+preprocessor_criteria.start(company_code)
+sentimental_analysis_criteria.analyze(company_code)
+make_criteria.get_criteria(company_code,days)
+#company_critera에 각 날짜별로 점수와 기사 개수가 저장됨
+#저장 순서 0,ratio,portion,기사개수,date
