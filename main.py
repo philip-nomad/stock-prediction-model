9dc9626d0b160d3bceafcde11179d7524eb664d0
mg <coalsrbs7@gmail.com>
2021-05-13 17:43:02 +0900

    delete: 주석처리된 내용 다시 복구

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ company_code_list = ['005930','035720','035420','000660']
 if __name__ == '__main__':
     for company_code in company_code_list:
         # 1. PER 정보 크롤링
-        #per_crawler.start(company_code)
+        per_crawler.start(company_code)
         # 2. 뉴스기사 크롤링
         news_contents_crawler.start(company_code)
         # 3. kosac 을 이용해 뉴스기사 데이터 전처리
-        #kosac_preprocessor.start(company_code)
+        kosac_preprocessor.start(company_code)
         # 4. 뉴스기사 감성분석
-        #news_contents_sentimental_analysis.analyze(company_code)
+        news_contents_sentimental_analysis.analyze(company_code)
         # 5. 주가예측
-        #predicted_value = prediction.predict(company_code_list)
+        predicted_value = prediction.predict(company_code_list)
 
-        #print(f"company_code: {company_code}")
-        #print(f"value_predicted: {predicted_value}%")
+        print(f"company_code: {company_code}")
+        print(f"value_predicted: {predicted_value}%")
