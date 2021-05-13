05624f892a3b5b3499690bd1caf87f023a0171f8
mg <coalsrbs7@gmail.com>
2021-05-13 17:38:50 +0900

    add: crawled data

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
-import kosac_preprocessor
 import news_contents_crawler
-import news_contents_sentimental_analysis
-import per_crawler
-import prediction
 
-company_code_list = ['005930','035720','035420','000660']
+company_code_list = ['005930', '035720', '035420', '000660']
 
 if __name__ == '__main__':
     for company_code in company_code_list:
         # 1. PER 정보 크롤링
-        #per_crawler.start(company_code)
+        # per_crawler.start(company_code)
         # 2. 뉴스기사 크롤링
         news_contents_crawler.start(company_code)
         # 3. kosac 을 이용해 뉴스기사 데이터 전처리
-        #kosac_preprocessor.start(company_code)
+        # kosac_preprocessor.start(company_code)
         # 4. 뉴스기사 감성분석
-        #news_contents_sentimental_analysis.analyze(company_code)
+        # news_contents_sentimental_analysis.analyze(company_code)
         # 5. 주가예측
-        #predicted_value = prediction.predict(company_code_list)
+        # predicted_value = prediction.predict(company_code_list)
 
-        #print(f"company_code: {company_code}")
-        #print(f"value_predicted: {predicted_value}%")
+        # print(f"company_code: {company_code}")
+        # print(f"value_predicted: {predicted_value}%")
--- news_contents_crawler.py
+++ news_contents_crawler.py
@@ -1,6 +1,6 @@
+import datetime
 import os
 import re
-from datetime import datetime
 
 import pandas as pd
 import requests
@@ -17,142 +17,76 @@ if not os.path.exists('score'):
 if not os.path.exists('words'):
     os.makedirs('words')
 
+TARGET_DATE = datetime.date(2021, 2, 1)
+
 
 def start(company_code):
-    unique_news_titles = {}
     if not os.path.exists("./date_news/" + company_code):
         os.makedirs("./date_news/" + company_code)
-    page_index = 0
+
+    unique_news_titles = set()
     page = 1
-    title_result = []
-    content_result = []
-    date_res = []
-    min_date = datetime(2021, 5, 13, 2, 30, 4).date()
     while True:
-        is_able_to_crawl = False
         url = 'https://finance.naver.com/item/news_news.nhn?code=' + str(company_code) + '&page=' + str(page)
+
         source_code = requests.get(url).text
         html = BeautifulSoup(source_code, "lxml")
 
-        tmp_data = []
+        processing_date = datetime.date.today()  # 오늘 날짜부터 시작
 
-        # is_able = []
-        new_date = datetime.now().date()
-
-        dates = [date.get_text() for date in html.select('.date')]
-        titles = html.select('.title')
-        link_result = []
-        links = html.select('.title')
+        dates = [datetime.datetime.strptime(date.get_text(), ' %Y.%m.%d %H:%M').date() for date in html.select('.date')]
+        titles = [re.sub('\n', '', str(title.get_text())) for title in html.select('.title')]
+        links = ['https://finance.naver.com' + link.find('a')['href'] for link in html.select('.title')]
 
         flag = True
 
-        for link in links:
-            add = 'https://finance.naver.com' + link.find('a')['href']
-            link_result.append(add)
-
         result_date = []
         result_title = []
-        result_content = []
+        result_contents = []
 
-        for row in list(zip(dates, titles, link_result)):
-            date: str = row[0]
-            title = re.sub('\n', '', str(row[1].get_text()))
-            link = str(row[2])
+        for row in list(zip(dates, titles, links)):
+            date = row[0]
+            title = row[1]
+            link = row[2]
 
             if title in unique_news_titles:
                 continue
 
             unique_news_titles.add(title)
 
-            url = link
-            source_code = requests.get(url).text
+            source_code = requests.get(link).text
             html = BeautifulSoup(source_code, "lxml")
-            contents = html.select("div#news_read")
-            text = str(contents)
-            text.find("<span")
-            a = text.find("<a")
-            text = remove_filename(text[0:a])
+            contents = str(html.select("div#news_read"))
+            contents.find("<span")
+            a = contents.find("<a")
+            contents = remove_filename(contents[0:a])
 
-            news_date = datetime.strptime(date, ' %Y.%m.%d %H:%M').date()
-            min_delta = new_date - min_date
-            if (new_date - news_date).days != 0:
-                for t in tmp_data:
-                    result_date.append(t[0])
-                    result_title.append(t[1])
-                    result_content.append(t[2])
-                result = {"날짜": result_date, "기사제목": result_title, "본문내용": result_content}
+            if (processing_date - date).days != 0:  # row 단위로 뉴스기사를 읽어오다가 날짜가 달라진 경우
+                result = {"날짜": result_date, "기사제목": result_title, "본문내용": result_contents}
                 df_result = pd.DataFrame(result)
-                df_result.to_csv("./date_news/" + company_code + "/" + company_code + "_" + str(new_date)[:10] + '.csv',
-                                 mode='w',
-                                 encoding='utf-8-sig')
-                new_date = news_date
-                result_date = []
-                result_title = []
-                result_content = []
-                tmp_data.clear()
+                df_result.to_csv(
+                    "./date_news/" + company_code + "/" + company_code + "_" + str(processing_date)[:10] + '.csv',
+                    mode='w',
+                    encoding='utf-8-sig'
+                )
+                processing_date = date
+                result_date.clear()
+                result_title.clear()
+                result_contents.clear()
 
-            news_result = [date, title, text]
-            tmp_data.append(news_result)
-
-            if min_delta.days < 0:
+            if (processing_date - TARGET_DATE).days < 0:  # 현재 읽어오려는 뉴스기사의 날짜가 원하는 날짜보다 더 과거의 날짜인 경우
                 flag = False
                 break
 
-        if flag is False:
+            result_date.append(date)
+            result_title.append(title)
+            result_contents.append(contents)
+
+        if not flag:
             break
 
-        '''
-
-        for date in dates:
-            date_compare = datetime.strptime(date, ' %Y.%m.%d %H:%M').date()
-            delta = date_compare - min_date
-            if delta >= 0:
-                is_able_to_crawl = True
-                is_able.append(True)
-                date_res.append(date_compare)
-            else:
-                is_able.append(False)
-
-        if not is_able_to_crawl:
-            break
-
-        titles = html.select('.title')
-        for idx, title in enumerate(titles):
-            if not is_able[idx]:
-                continue
-            title = title.get_text()
-            title = re.sub('\n', '', title)
-            title_result.append(title)
-
-        # 뉴스 링크
-        link_result = []
-        links = html.select('.title')
-
-        for link in links:
-            add = 'https://finance.naver.com' + link.find('a')['href']
-            link_result.append(add)
-
-        for idx, link in enumerate(link_result):
-            if not is_able[idx]:
-                continue
-            url = link
-            source_code = requests.get(url).text
-            html = BeautifulSoup(source_code, "lxml")
-            contents = html.select("div#news_read")
-            text = str(contents)
-            text.find("<span")
-            a = text.find("<a")
-            text = remove_filename(text[0:a])
-            content_result.append(text)
-        '''
-        page_index += 1
+        print(f"company_code: {company_code}, processing_date: {processing_date}, 크롤링 끝난 페이지: {page}")
         page += 1
-        print(f"크롤링 페이지: {page_index}")
-
-    # result = {"날짜": date_res, "기사제목": title_result, "본문내용": content_result}
-    # df_result = pd.DataFrame(result)
-    # print(f"{company_code} 기사를 다운 받고 있습니다------")
-    # df_result.to_csv("./news/" + company_code + '.csv', mode='w', encoding='utf-8-sig')
 
 
 # html 태그 제거하는 코드
