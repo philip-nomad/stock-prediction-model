39c8a2a383cb481a0dd109b3463d135a5c110b39
Jaehyung <godjaehyung@gmail.com>
2021-05-31 00:32:03 +0900

    make closing_calculate.py

---
+++ closing_calculate.py
@@ -0,0 +1,47 @@
+import csv
+import os
+import lstm_calculator
+import news_contents_sentimental_analysis
+
+PATH = "./"
+os.chdir(PATH)
+DIR = 'prediction_score'
+STOCK_DIR = 'stock'
+
+def predict(company_code, predict_date, w1, w2, w3):
+    with open(f"./{lstm_calculator.DIR}/{company_code}/{company_code}_{predict_date}.csv", 'r', -1,
+              'utf-8') as lines:
+        next(lines)
+
+        for line in csv.reader(lines):
+            lstm_prediction = round(float(line[0]))
+            previous_closing_price = round(float(line[1]))
+
+    lstm_value = (lstm_prediction - previous_closing_price) / previous_closing_price  # (다음날 예측 종가 - 오늘 종가) / 오늘 종가
+    # 감성분석 값 불러오기
+    emotional_analysis_csv = news_contents_sentimental_analysis.calculate_two_weeks(company_code,
+                                                                                    predict_date)
+
+    # PER 값 불러오기
+    company_per_csv = 0
+    same_category_per_csv = 0
+    with open('./per_data/' + company_code + '.csv', 'r', -1, 'utf-8') as lines:
+        next(lines)
+
+        for line in csv.reader(lines):
+            company_per_csv = float(line[3])
+            same_category_per_csv = float(line[4])
+
+    # 자기 회사 PER 이랑 동일업종 PER 이 모두 양수인 경우에만 per_value 계산
+    if company_per_csv > 0 and same_category_per_csv > 0:
+        per_value_csv = 1 - company_per_csv / same_category_per_csv  # 1 - 자기 회사 PER / 동일 업종 PER
+    else:
+        per_value_csv = 0
+
+
+
+    predicted_value = 100 * (lstm_value * w1 +
+                             emotional_analysis_csv * w2 +
+                             per_value_csv * w3)
+
+    return predicted_value
